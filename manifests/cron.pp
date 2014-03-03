bc1500a3465aa961bef996c4490ed2d94698456d
fschaer <frederic.schaer@cea.fr>
2014-03-03 15:09:57 +0100

    add cron logging

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -2,7 +2,7 @@ class apelparser::cron (
   $cron_minutes = 35,
   $cron_hours   = 1,) {
   cron { apelparser:
-    command => "/usr/bin/apelparser",
+    command => "/usr/bin/apelparser >> /var/log/apelparser.log 2>&1",
     user    => root,
     hour    => $cron_hours,
     minute  => $cron_minutes,
